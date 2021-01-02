7669d078b3984d2e5be1236b3940d4d9850d1c1f
Dmitry Brant <dbrant@wikimedia.org>
2021-01-02 16:45:32 -0500

    Make it into a clock.

--- pictureframe.py
+++ pictureframe.py
@@ -8,14 +8,25 @@ if os.path.exists(libpath):
     sys.path.append(libpath)
 
 import logging
+import datetime
+import random
 from waveshare_epd import epd7in5_V2
 import time
 from PIL import Image,ImageDraw,ImageFont
 
 logging.basicConfig(level=logging.DEBUG)
 
+epd = epd7in5_V2.EPD()
+
+imageList = [ 'db1.jpg', 'bissel3.jpg', 'db2.jpg', 'bissel2.jpg' ]
+
+totalWidth = 800
+totalHeight = 480
+clockWidth = 320
+clockHeight = 150
+phase = 0
+
 try:
-    epd = epd7in5_V2.EPD()
     logging.info("Initializing...")
     epd.init()
 
@@ -24,18 +35,48 @@ try:
     #epd.Clear()
     #logging.info("That took " + str(int(round(time.time() * 1000)) - millis) + " ms")
 
-    millis = int(round(time.time() * 1000))
-    logging.info("Loading image...")
-    img = Image.open(os.path.join(imagepath, 'db1.jpg'))
-    img = img.convert(mode='1',dither=Image.FLOYDSTEINBERG)
-    epd.display(epd.getbuffer(img))
-    logging.info("That took " + str(int(round(time.time() * 1000)) - millis) + " ms")
+    while True:
+        logging.info("Loading next image...")
 
-    logging.info("Going to sleep...")
-    epd.sleep()
-    epd.Dev_exit()
+        imageName = random.choice(imageList)
+
+        now = datetime.datetime.now()
+        img = Image.open(os.path.join(imagepath, imageName))
+
+        draw = ImageDraw.Draw(img)
+        clockFont = ImageFont.truetype('agenda.ttf', 128)
+        clockText = now.strftime("%H:%M")
+
+        if phase == 0:
+            draw.rectangle((0, 0, clockWidth, clockHeight), fill = 'white')
+            draw.text((24, 0), clockText, font = clockFont, fill = 0)
+        elif phase == 1:
+            draw.rectangle((totalWidth - clockWidth, 0, totalWidth, clockHeight), fill = 'white')
+            draw.text((totalWidth - clockWidth + 24, 0), clockText, font = clockFont, fill = 0)
+        elif phase == 2:
+            draw.rectangle((0, totalHeight - clockHeight, clockWidth, totalHeight), fill = 'white')
+            draw.text((24, totalHeight - clockHeight), clockText, font = clockFont, fill = 0)
+        elif phase == 3:
+            draw.rectangle((totalWidth - clockWidth, totalHeight - clockHeight, totalWidth, totalHeight), fill = 'white')
+            draw.text((totalWidth - clockWidth + 24, totalHeight - clockHeight), clockText, font = clockFont, fill = 0)
+
+        img = img.convert(mode='1',dither=Image.FLOYDSTEINBERG)
+
+        img.save("foo.png", "PNG")
+
+        img = img.convert(mode='1',dither=Image.FLOYDSTEINBERG)
+        epd.display(epd.getbuffer(img))
+
+        epd.sleep()
+
+        time.sleep(60)
+
+        phase = phase + 1
+        if phase > 3:
+            phase = 0
 
 except KeyboardInterrupt:    
     logging.info("ctrl + c:")
+    epd.Dev_exit()
     epd7in5_V2.epdconfig.module_exit()
     exit()
